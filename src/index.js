604cc99150eab9e2cc4c029ec45e46d67c8e8c10
binaryArrow <cancihan96@gmail.com>
2022-04-09 03:47:57 +0200

    add: register commands TODO: add commands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Client, Intents} = require('discord.js')
+const { Client, Intents} = require('discord.js');
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]})
 
