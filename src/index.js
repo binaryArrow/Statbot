c70ae6f3367c5855b70f2cb43efad72159d072a0
binaryArrow <cancihan96@gmail.com>
2022-04-10 00:02:41 +0200

    Ref.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { Client, Intents} from 'discord.js'
 
 dotenv.config()
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS]})
-
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES]})
+let users = []
 client.once('ready', ()=>{
     console.log('ready!')
 })
+
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -18,11 +19,20 @@ client.on('interactionCreate', async interaction => {
     if (commandName === 'ping') {
         await interaction.reply('Pong');
     } else if (commandName === 'server') {
-        await interaction.reply('Server info.');
+        await interaction.reply(
+            `Server name: ${interaction.guild.name}\nToal members: ${interaction.guild.memberCount}`
+        );
     } else if (commandName === 'user') {
         await interaction.reply('User info.');
     }
 });
+
+client.on('voiceStateUpdate',(oldState, newState) =>{
+    if(oldState.channelId === null && newState.channelId !== null){
+        console.log(oldState.member.displayName)
+    }
+})
+
 client.login(process.env.DISCORD_TOKEN).then(()=>{
     console.log('logged in!')
 })
