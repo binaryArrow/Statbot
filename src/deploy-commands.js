ce8992cc2fb5c91ff9f4576f8e700f610f990d49
binaryArrow <cancihan96@gmail.com>
2022-04-09 04:16:28 +0200

    ref.

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,11 +1,11 @@
+const {SlashCommandBuilder} = require('@discordjs/builders')
+const {REST} = require('@discordjs/rest')
+const {Routes} = require('discord-api-types/v9')
 require('dotenv').config()
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 
-const clientId = process.env.CLIENT_ID
-const token = process.env.DISCORD_TOKEN
-const guildId = process.env.SERVER_ID
+const CLIENT_ID = process.env.CLIENT_ID
+const TOKEN = process.env.DISCORD_TOKEN
+const GUILD_ID = process.env.SERVER_ID
 
 const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
@@ -14,8 +14,10 @@ const commands = [
 ]
     .map(command => command.toJSON());
 
-const rest = new REST({version: '9'}).setToken(token);
+const rest = new REST({version: '10'}).setToken(TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
+
+// rest.get(Routes.guildChannels(GUILD_ID)).then((res)=>console.log(res)).catch(console.error)
